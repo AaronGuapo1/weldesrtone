1a5e9230075b5a6c07b5e9fc6182181d6d1e0d5b
Kaldo <dantekaled100@gmail.com>
2023-01-07 17:38:31 -0600

    Passport bug

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const MicrosoftStrategy = require('passport-microsoft').Strategy;
+const User = require("./models/User.js")
 
 // -------------- MIDDLEWARE -------------- //
 function nocache(req, res, next) { /// function used to remove cache anywhere needed
@@ -31,6 +36,87 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// -------------- PASSPORT CONFIG -------------- //
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        role: user.role,
+        picture: user.picture
+      });
+    });
+});  
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+});
+
+// - Google OAuth
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/welderstone"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, async function (err, user) {
+      if(user?.role == undefined){
+        await User.updateMany({googleId: user.googleId}, {username: profile.displayName, role: "costumer"});
+      }
+
+      return cb(err, user);
+    });
+  }
+));
+
+// - Facebook OAuth
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/welderstone"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ facebookId: profile.id }, async function (err, user) {
+    if(user?.role == undefined){
+      await User.updateMany({facebookId: profile.id}, {username: profile.displayName, role: "costumer"});
+    }
+
+    return cb(err, user);
+  });
+}
+));
+
+passport.use(new MicrosoftStrategy({
+  // Standard OAuth2 options
+  clientID: process.env.MICROSOFT_APP_ID,
+  clientSecret: process.env.MICROSOFT_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/microsoft/welderstone",
+  scope: ['user.read'],
+
+  // Microsoft specific options
+
+  // [Optional] The tenant for the application. Defaults to 'common'. 
+  // Used to construct the authorizationURL and tokenURL
+  tenant: 'common',
+
+  // [Optional] The authorization URL. Defaults to `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize`
+  authorizationURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+
+  // [Optional] The token URL. Defaults to `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`
+  tokenURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+},
+function(accessToken, refreshToken, profile, done) {
+  User.findOrCreate({ microsftId: profile.id }, async function (err, user) {
+    if(user?.role == undefined){
+      await User.updateMany({facebookId: profile.id}, {username: profile.displayName, role: "costumer"});
+    }
+    return done(err, user);
+  });
+}
+));
 
 // ---------------- DATABASE ---------------- // 
 mongoose.set('strictQuery', true);
--- models/User.js
+++ models/User.js
@@ -8,7 +8,6 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 const MicrosoftStrategy = require('passport-microsoft').Strategy;
 
-
 // -------------- SCHEMA -------------- //
 const userSchema = new Schema({
     username: String,
@@ -25,86 +24,5 @@ userSchema.plugin(findOrCreate);
     
 // -------------- MODEL -------------- //
 const User = mongoose.model("User", userSchema);
-
-// -------------- USER CONFIG -------------- //
-passport.use(User.createStrategy());
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, {
-        id: user.id,
-        username: user.username,
-        role: user.role,
-        picture: user.picture
-      });
-    });
-});  
-passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
-});
-
-// - Google OAuth
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/welderstone"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, async function (err, user) {
-      if(user?.role == undefined){
-        await User.updateMany({googleId: user.googleId}, {username: profile.displayName, role: "costumer"});
-      }
-
-      return cb(err, user);
-    });
-  }
-));
-
-// - Facebook OAuth
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/welderstone"
-},
-function(accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ facebookId: profile.id }, async function (err, user) {
-    if(user?.role == undefined){
-      await User.updateMany({facebookId: profile.id}, {username: profile.displayName, role: "costumer"});
-    }
-
-    return cb(err, user);
-  });
-}
-));
-
-passport.use(new MicrosoftStrategy({
-  // Standard OAuth2 options
-  clientID: process.env.MICROSOFT_APP_ID,
-  clientSecret: process.env.MICROSOFT_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/microsoft/welderstone",
-  scope: ['user.read'],
-
-  // Microsoft specific options
-
-  // [Optional] The tenant for the application. Defaults to 'common'. 
-  // Used to construct the authorizationURL and tokenURL
-  tenant: 'common',
-
-  // [Optional] The authorization URL. Defaults to `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize`
-  authorizationURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-
-  // [Optional] The token URL. Defaults to `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`
-  tokenURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-},
-function(accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ microsftId: profile.id }, async function (err, user) {
-    if(user?.role == undefined){
-      await User.updateMany({facebookId: profile.id}, {username: profile.displayName, role: "costumer"});
-    }
-    return done(err, user);
-  });
-}
-));
 
 module.exports = User
