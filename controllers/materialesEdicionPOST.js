895faf8ebba5f27188debea8eec9d34369e945bf
AaronGuapo1 <mraaronrodriguez@hotmail.com>
2023-01-23 02:10:54 -0600

    detalles

--- controllers/materialesEdicionPOST.js
+++ controllers/materialesEdicionPOST.js
@@ -1,9 +1,11 @@
 const material = require('../models/materiales.js');
+const Producto = require('../models/Productos.js');
+const path = require('path');
 
 
 module.exports = async (req,res)=>{        
     const obj_ids = req.body.id;
-    console.log(req.body);
+    //console.log(req.body);
 
 
     if(obj_ids != undefined ){
@@ -32,7 +34,7 @@ module.exports = async (req,res)=>{
                     params.SubFam = req.body.SubFam[i];
                 }
     
-                console.log(params);
+                //console.log(params);
     
                 await material.findByIdAndUpdate(obj_ids[i], params);
             }
@@ -65,9 +67,84 @@ module.exports = async (req,res)=>{
         }
     }
 
+const mateprod= await material.find({});
+const cantidadrespetada = await Producto.find({});
+for (let i=0; i<cantidadrespetada.length; i++){
+for (let a=0; a<mateprod.length; a++){
 
 
-//await Producto.updateOne({nombre:req.body.ProductoAEditar}, { $set: {"MaterialesProductos.$[item]":{Descripcion:req.body['MaterialesProductos[nombre]'][a],cantidad:req.body['MaterialesProductos[cantidad]'][a],codigo:req.body['MaterialesProductos[codigo]'][a],familia:req.body['MaterialesProductos[familia]'][a],preciounitario:req.body['MaterialesProductos[preciounitario]'][a]}}}, {arrayFilters: [{"item.codigo":req.body['MaterialesProductos[codigo]'][a]}]});
+await Producto.updateOne({_id:cantidadrespetada[i]._id}, { $set: {"MaterialesProductos.$[item]":{Descripcion:mateprod[a].Descripcion,cantidad:cantidadrespetada[i].MaterialesProductos[a].cantidad,codigo:mateprod[a].Codigo,familia:mateprod[a].Familia,preciounitario:mateprod[a].PrecioUnitario}}},{arrayFilters: [{"item._id":cantidadrespetada[i].MaterialesProductos[a]._id}]});
+await Producto.updateOne({_id:cantidadrespetada[i]._id}, { $set: {"PinturaProductos.$[item]":{Descripcion:mateprod[a].Descripcion,cantidad:cantidadrespetada[i].PinturaProductos[a].cantidad,codigo:mateprod[a].Codigo,familia:mateprod[a].Familia,preciounitario:mateprod[a].PrecioUnitario}}},{arrayFilters: [{"item._id":cantidadrespetada[i].PinturaProductos[a]._id}]});
+await Producto.updateOne({_id:cantidadrespetada[i]._id}, { $set: {"InstalacionProductos.$[item]":{Descripcion:mateprod[a].Descripcion,cantidad:cantidadrespetada[i].InstalacionProductos[a].cantidad,codigo:mateprod[a].Codigo,familia:mateprod[a].Familia,preciounitario:mateprod[a].PrecioUnitario}}},{arrayFilters: [{"item._id":cantidadrespetada[i].InstalacionProductos[a]._id}]});
+
+
+
+}
+}
+
+
+
+const MaterialesASumar = await Producto.find({})
+
+for (let a =0; a< MaterialesASumar.length; a++){
+const {MaterialesProductos} = MaterialesASumar[a];
+const {PinturaProductos} = MaterialesASumar[a];
+const {InstalacionProductos} = MaterialesASumar[a];
+
+var suma = 0;
+
+for (let i=0; i<MaterialesProductos.length; i++){
+        if (MaterialesProductos[i].preciounitario >=0){
+         suma = suma + (MaterialesProductos[i].preciounitario * MaterialesProductos[i].cantidad)
+        }
+}
+
+var Suma2Mano= ((suma * MaterialesASumar[a].ManoObMaterial)/100) + suma;
+var Suma3Por= ((Suma2Mano * MaterialesASumar[a].PorcentajeMaterial)/100) +Suma2Mano;
+
+
+var sumaSolventes = 0;
+            
+for (let m=0; m<PinturaProductos.length; m++){
+    if (PinturaProductos[m].preciounitario >=0){
+
+        sumaSolventes = sumaSolventes + (PinturaProductos[m].preciounitario * PinturaProductos[m].cantidad);
+    }
+    }
+
+var sumaSolventes2Mano= ((sumaSolventes * MaterialesASumar[a].ManoObPintura)/100) + sumaSolventes;
+var sumaSolventes3Por=((sumaSolventes2Mano * MaterialesASumar[a].PorcentajePintura)/100) +sumaSolventes2Mano;
+
+
+    var sumaInsumos = 0;
+            
+    for (let n=0; n<InstalacionProductos.length; n++){
+
+        if (InstalacionProductos[n].preciounitario >=0){
+
+            sumaInsumos = sumaInsumos + (InstalacionProductos[n].preciounitario * InstalacionProductos[n].cantidad);
+        }
+
+         }
+
+         var sumaInsumos2Mano = ((sumaInsumos * MaterialesASumar[a].ManoObInstalacion)/100) + sumaInsumos;
+         var sumaInsumos3Por = ((sumaInsumos * MaterialesASumar[a].PorcentajeInstalacion)/100) + sumaInsumos2Mano;
+
+
+         var x = Suma3Por+sumaSolventes3Por+sumaInsumos3Por;
+         var SubTotal=Math.round(x)
+         console.log(SubTotal)
+         await Producto.updateOne({_id:MaterialesASumar[a]._id},{ $set: { precio:SubTotal } });
+
+
+
+
+
+        }
+
+
+
+
 
 
 
@@ -77,4 +154,15 @@ module.exports = async (req,res)=>{
 
 
 
-//await Producto.updateOne({nombre:req.body.ProductoAEditar}, { $set: {"MaterialesProductos.$[item]":{Descripcion:req.body['MaterialesProductos[nombre]'][a],cantidad:req.body['MaterialesProductos[cantidad]'][a],codigo:req.body['MaterialesProductos[codigo]'][a],familia:req.body['MaterialesProductos[familia]'][a],preciounitario:req.body['MaterialesProductos[preciounitario]'][a]}}}, {arrayFilters: [{"item.codigo":req.body['MaterialesProductos[codigo]'][a]}]});
+
+
+
+/*
+
+
+
+
+
+
+
+*/
--- controllers/productosEMPost.js
+++ controllers/productosEMPost.js
@@ -26,6 +26,68 @@ for (c=0; c<req.body['InstalacionProductos[cantidad]'].length;c++){
 
 }
 
+const MaterialesASumar = await Producto.find({})
+
+for (let a =0; a< MaterialesASumar.length; a++){
+const {MaterialesProductos} = MaterialesASumar[a];
+const {PinturaProductos} = MaterialesASumar[a];
+const {InstalacionProductos} = MaterialesASumar[a];
+
+var suma = 0;
+
+for (let i=0; i<MaterialesProductos.length; i++){
+        if (MaterialesProductos[i].preciounitario >=0){
+         suma = suma + (MaterialesProductos[i].preciounitario * MaterialesProductos[i].cantidad)
+        }
+}
+
+var Suma2Mano= ((suma * MaterialesASumar[a].ManoObMaterial)/100) + suma;
+var Suma3Por= ((Suma2Mano * MaterialesASumar[a].PorcentajeMaterial)/100) +Suma2Mano;
+
+
+var sumaSolventes = 0;
+            
+for (let m=0; m<PinturaProductos.length; m++){
+    if (PinturaProductos[m].preciounitario >=0){
+
+        sumaSolventes = sumaSolventes + (PinturaProductos[m].preciounitario * PinturaProductos[m].cantidad);
+    }
+    }
+
+var sumaSolventes2Mano= ((sumaSolventes * MaterialesASumar[a].ManoObPintura)/100) + sumaSolventes;
+var sumaSolventes3Por=((sumaSolventes2Mano * MaterialesASumar[a].PorcentajePintura)/100) +sumaSolventes2Mano;
+
+
+    var sumaInsumos = 0;
+            
+    for (let n=0; n<InstalacionProductos.length; n++){
+
+        if (InstalacionProductos[n].preciounitario >=0){
+
+            sumaInsumos = sumaInsumos + (InstalacionProductos[n].preciounitario * InstalacionProductos[n].cantidad);
+        }
+
+         }
+
+         var sumaInsumos2Mano = ((sumaInsumos * MaterialesASumar[a].ManoObInstalacion)/100) + sumaInsumos;
+         var sumaInsumos3Por = ((sumaInsumos * MaterialesASumar[a].PorcentajeInstalacion)/100) + sumaInsumos2Mano;
+
+
+         var x = Suma3Por+sumaSolventes3Por+sumaInsumos3Por;
+         var SubTotal=Math.round(x)
+         console.log(SubTotal)
+         await Producto.updateOne({_id:MaterialesASumar[a]._id},{ $set: { precio:SubTotal } });
+
+
+
+
+
+        }
+
+
+
+
+
 res.redirect('/productos');
 
 }
