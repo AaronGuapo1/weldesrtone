8a5e338b2a157aecc21be1e2dd554abf8b531e73
AaronGuapo1 <mraaronrodriguez@hotmail.com>
2023-02-24 18:50:39 -0600

    faltante

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/welderstone"
+    callbackURL: "/https://welderstoneprueba.onrender.com/auth/google/welderstone"
     //http://localhost:3000/auth/google/welderstone
     //https://welderstoneprueba.onrender.com/auth/google/welderstone
   },
