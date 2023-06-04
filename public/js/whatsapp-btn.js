5a264dae86ff81f1afb92192d317e886e157972b
Kaldo <dantekaled100@gmail.com>
2023-06-04 15:28:43 -0600

    Cambio en el boton de Whatsapp

--- public/js/whatsapp-btn.js
+++ public/js/whatsapp-btn.js
@@ -1,8 +1,8 @@
 const whatsappBtn = document.getElementById("whatsapp-btn");
 
 whatsappBtn.addEventListener("click", () => {
-    const phoneNumber = "123456789"; // Reemplaza esto con tu número de teléfono
-    const message = "Hola, ¿cómo puedo ayudarte?"; // Reemplaza esto con el mensaje que quieras enviar
+    const phoneNumber = "8712174679"; // Reemplaza esto con tu número de teléfono
+    const message = "¡Escribe aquí como podemos ayudarte!"; // Reemplaza esto con el mensaje que quieras enviar
     
     const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
     
